f4bfe6fcc2903be0e6fec08e8e7ec8465fbe4e7e
nathezek <nathezek7@gmail.com>
2025-12-02 10:02:41 +0300

    just changed the styling and add a version number v.1.0 on the title CLI Calculator

--- calculator.rs
+++ calculator.rs
@@ -1,22 +1,21 @@
-
 use std::io;
 use std::io::Write;
 
 fn main() {
     // CLI Calculator //
-    println!("CLI Calculator 🧮");
+    println!("CLI Calculator v.1.0 🧮");
 
     print!("Enter first number: ");
     io::stdout().flush().expect("Flushing failed");
 
     // first_number //
-    let mut first_number = String::new(); 
+    let mut first_number = String::new();
 
-    io::stdin().read_line( &mut first_number)
+    io::stdin()
+        .read_line(&mut first_number)
         .expect("Failed to read_line!");
 
-    let first_number :f64 = first_number.trim().parse()
-        .expect("Failed to type number!");
+    let first_number: f64 = first_number.trim().parse().expect("Failed to type number!");
 
     // println!("You entered: {}", first_number);
 
@@ -25,30 +24,33 @@ fn main() {
     io::stdout().flush().expect("Flushing failed");
     let mut second_number = String::new();
 
-    io::stdin().read_line( &mut second_number)
+    io::stdin()
+        .read_line(&mut second_number)
         .expect("Failed to read_line");
 
-    let second_number :f64 = second_number.trim().parse()
+    let second_number: f64 = second_number
+        .trim()
+        .parse()
         .expect("Failed to type number!");
 
     // println!("You entered: {}", second_number);
 
-
     // choose an operation //
     println!("Choose an operation ['add or +' 'subtract or -' 'multiply or *' 'divide or /'] :");
     let mut operator_input = String::new();
-    io::stdin().read_line( &mut operator_input)
+    io::stdin()
+        .read_line(&mut operator_input)
         .expect("Failed to read_line!");
-
 
     let operation_choice = Operation::from_str(&operator_input);
 
-
-
     // show results
-    let results = operator(operation_choice.expect("invalid!") ,first_number, second_number);
+    let results = operator(
+        operation_choice.expect("invalid!"),
+        first_number,
+        second_number,
+    );
     println!("Result is: {}", results);
-
 }
 
 enum Operation {
@@ -59,8 +61,7 @@ enum Operation {
 }
 
 impl Operation {
-    fn from_str(operator_input :&str) -> Option<Self> {
-
+    fn from_str(operator_input: &str) -> Option<Self> {
         match operator_input.trim().to_lowercase().as_str() {
             "add" | "+" => Some(Operation::Add),
             "subtract" | "-" => Some(Operation::Subtract),
@@ -71,9 +72,7 @@ impl Operation {
     }
 }
 
-
-fn operator(op: Operation, first_number :f64, second_number :f64) -> f64 {
-    
+fn operator(op: Operation, first_number: f64, second_number: f64) -> f64 {
     match op {
         Operation::Add => first_number + second_number,
         Operation::Subtract => first_number - second_number,
@@ -81,3 +80,4 @@ fn operator(op: Operation, first_number :f64, second_number :f64) -> f64 {
         Operation::Divide => first_number / second_number,
     }
 }
+
